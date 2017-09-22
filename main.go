c669bd2e7c620c47843519beb97e828b8d748efc
Arash Outadi <arash.outadi@ravelin.com>
2017-09-22 16:16:56 +0100

    First commit RoboRooney!

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/nlopes/slack"
+)
+
+//go:generate echo '{"apiToken":"","channelId":""}' > config.json
+
+func main() {
+	if len(os.Args) == 3 {
+
+	}
+
+	cred := &Credentials{}
+	cred.Read()
+
+	api := slack.New(cred.APIToken)
+
+	logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
+	slack.SetLogger(logger)
+	api.SetDebug(false)
+
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		fmt.Print("Event Received: ")
+		switch ev := msg.Data.(type) {
+		case *slack.HelloEvent:
+			// Ignore hello
+
+		case *slack.ConnectedEvent:
+			fmt.Println("Infos:", ev.Info)
+			fmt.Println("Connection counter:", ev.ConnectionCount)
+
+		case *slack.MessageEvent:
+			fmt.Printf("Message: %v\n", ev.Msg.Text)
+			rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", cred.ChannelID))
+
+		case *slack.PresenceChangeEvent:
+			fmt.Printf("Presence Change: %v\n", ev)
+
+		case *slack.LatencyReport:
+			fmt.Printf("Current latency: %v\n", ev.Value)
+
+		case *slack.RTMError:
+			fmt.Printf("Error: %s\n", ev.Error())
+
+		case *slack.InvalidAuthEvent:
+			fmt.Printf("Invalid credentials")
+			return
+
+		default:
+
+			// Ignore other events..
+			// fmt.Printf("Unexpected: %v\n", msg.Data)
+		}
+	}
+}
